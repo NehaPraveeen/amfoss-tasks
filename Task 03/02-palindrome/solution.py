02e2a55251c77f28a5f22d8e818285acf462974a
NehaPraveeen <nehapraveen208@gmail.com>
2026-08-28 00:40:30 +0530

    palindrome problem --> solution
    Fix palindrome check by reversing the string representation of the integer.

--- Task 03/02-palindrome/solution.py
+++ Task 03/02-palindrome/solution.py
@@ -1,4 +1,17 @@
+# given an integer x, return true if x is a palindrome, and false otherwise.
 
+class Solution(object):
+    def isPalindrome(self, x):
+        self=str(x)
+        new=self[::-1]
+        if new==self:
+            return True
+        else:
+            return False
+
+
+
+        
 
 
 
