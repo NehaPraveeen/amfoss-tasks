3b36b58cc64e71068975b3767d77c25c944b85dc
NehaPraveeen <nehapraveen208@gmail.com>
2026-08-28 01:29:41 +0530

    Roman to Integer problem-->solution

--- Task 03/03-Roman to Integer/solution.py
+++ Task 03/03-Roman to Integer/solution.py
@@ -1,1 +1,11 @@
-
+# Convert the given Roman Number to Integer
+class Solution(object):
+    def romanToInt(self, s):
+        n=0
+        values={"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        for i in range(len(s)):
+            if i+1<len(s) and values[s[i]]<values[s[i+1]]:
+                n-=values[s[i]]
+            else:
+                n+=values[s[i]]
+        return n
