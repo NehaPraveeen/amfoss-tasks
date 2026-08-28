89eb2c62ff0fc84b5434e598ae0d7ac831ba6f9f
NehaPraveeen <nehapraveen208@gmail.com>
2026-08-28 15:31:47 +0530

    Reversing Num problem --> solution
    Implement a method to reverse an integer, handling both positive and negative values. Ensure the result is within the 32-bit signed integer range.

--- Task 03/04-Reversing Number/solution.py
+++ Task 03/04-Reversing Number/solution.py
@@ -1,1 +1,22 @@
+# Reverse the number given (for both negetive and positive numbers)
+class Solution(object):
+    def reverse(self, x):
+        rev=0
+        if x>=0:
+            sign=1
+        else:
+            sign=-1
+        x=abs(x)
+        while x>0:
+            digit=x%10
+            rev=(rev*10)+digit
+            x=x//10
 
+        rev=rev*sign
+
+        if rev<-2**31 or rev>2**31 -1:
+            return 0
+        return rev
+
+
+        
